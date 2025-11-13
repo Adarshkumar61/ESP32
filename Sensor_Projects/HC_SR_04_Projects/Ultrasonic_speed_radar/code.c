070e484cdf466d0e57acfd096df42eebd1bf1f1a
Adarshkumar61 <adarshb2k61@gmail.com>
2025-11-13 13:32:11 +0530

    updated

--- Sensor_Projects/HC_SR_04_Projects/Ultrasonic_speed_radar/code.c
+++ Sensor_Projects/HC_SR_04_Projects/Ultrasonic_speed_radar/code.c
@@ -3,3 +3,19 @@
 #include <Wire.h>
 
 // --- Display setup (for 0.96" I2C OLED) ---
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// --- Pins ---
+#define trigPin 5
+#define echoPin 18 // i used 3.3V tolerant pin
+#define ledPin  2
+#define buzzerPin 4
+
+// --- Variables ---
+long duration;
+float distanceCm = 0, lastDistance = 0;
+unsigned long lastTime = 0;
+float speedKmph = 0;
+float speedLimit = 10.0;
+
+// --- Function prototypes ---
