2b2bc3e65d5514f2422f738c633351aa7158ae9e
Adarshkumar61 <adarshb2k61@gmail.com>
2025-11-13 13:32:37 +0530

    updated

--- Sensor_Projects/HC_SR_04_Projects/Ultrasonic_speed_radar/code.c
+++ Sensor_Projects/HC_SR_04_Projects/Ultrasonic_speed_radar/code.c
@@ -19,3 +19,39 @@ float speedKmph = 0;
 float speedLimit = 10.0;
 
 // --- Function prototypes ---
+float getDistance();
+void beepBuzzer(int times);
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C speed (faster)
+  
+  u8g2.begin();
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tf);
+  u8g2.drawStr(10, 30, "Speed Radar Ready");
+  u8g2.sendBuffer();
+  delay(1000);
+}
+
+void loop() {
+  float currentDistance = getDistance();
+  unsigned long currentTime = millis();
+
+  // Calculate speed
+  if (currentDistance > 0 && lastDistance > 0) {
+    float deltaDist = fabs(currentDistance - lastDistance);
+    float deltaTime = (currentTime - lastTime) / 1000.0;
+    if (deltaTime > 0.05 && deltaTime < 1.0) {
+      float speedCmPerSec = deltaDist / deltaTime;
+      speedKmph = speedCmPerSec * 0.036;
+    }
+  }
+
