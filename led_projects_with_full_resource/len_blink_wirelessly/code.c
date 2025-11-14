760838c99590083d8fd91db84ecb80b6c2bfaa69
Adarshkumar61 <adarshb2k61@gmail.com>
2025-11-14 10:29:53 +0530

    full code uploaded

--- led_projects_with_full_resource/len_blink_wirelessly/code.c
+++ led_projects_with_full_resource/len_blink_wirelessly/code.c
@@ -12,3 +12,23 @@ void setup() {
   server.begin();
 }
 
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) return;
+
+  String req = client.readStringUntil('\r');
+  client.flush();
+
+  if (req.indexOf("/ON") != -1) digitalWrite(ledPin, HIGH);
+  else if (req.indexOf("/OFF") != -1) digitalWrite(ledPin, LOW);
+
+  // Simple webpage
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html\r\n\r\n");
+  client.println("<!DOCTYPE html><html><body><center>");
+  client.println("<h2>ESP32 LED Control</h2>");
+  client.println("<a href='/ON'><button>ON</button></a>");
+  client.println("<a href='/OFF'><button>OFF</button></a>");
+  client.println("</center></body></html>");
+  client.stop();
+}
