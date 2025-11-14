fc8087508562764144e03aa0aef1ed3fc89587d9
Adarshkumar61 <adarshb2k61@gmail.com>
2025-11-14 10:29:24 +0530

    updated

--- led_projects_with_full_resource/len_blink_wirelessly/code.c
+++ led_projects_with_full_resource/len_blink_wirelessly/code.c
@@ -6,3 +6,9 @@ const char* password = "adarsh1234";
 WiFiServer server(80);
 const int ledPin = 2;
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  WiFi.softAP(ssid, password);
+  server.begin();
+}
+
